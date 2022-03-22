5a9363fe158049d0b28d8c2f4168a901225708f4
Desmond <desmondmies@gmail.com>
2022-03-22 20:22:06 +0100

    dijkstra fonctionnel?
    cleanup du code, ajout nombre de passe de moyenne médian pour la matrice, et dijkstra renvoi le chemin entre 2 pts

--- CaseMatrice.py
+++ CaseMatrice.py
@@ -1,61 +1,69 @@
 class CaseMatrice():
-    """
-    value = valeur de la case de la matrice (valeur d'élévation)\n
-    x = indice i de la position de cette case dans la matrice\n
-    y = indice j de la position de cette case dans la matrice\n
-    """
-    def __init__(self, value, x, y):
-        self._value = value
-        self._x = x
-        self._y = y
+	"""
+	value = valeur de la case de la matrice (valeur d'élévation)\n
+	x = indice i de la position de cette case dans la matrice\n
+	y = indice j de la position de cette case dans la matrice\n
+	"""
+	def __init__(self, value, x, y):
+		self._value = value
+		self._x = x
+		self._y = y
 
-        #prévention pour A*
-        #self.gCost = 0 
-        #self.hCost = 0
+		#prévention pour A*
+		#self.gCost = 0
+		#self.hCost = 0
 
-    def __repr__(self):
-        return "(" + str(self.X) + "," + str(self.Y) + " : " + str(self.Value) + ")"
+	def __repr__(self):
+		#return "(" + str(self.X) + "," + str(self.Y) + " : " + str(self.Value) + ")"
+		return str(self.Value)
 
-    def __lt__(self, other):
-        return (self.Value < other.Value)
+	def __lt__(self, other):
+		if isinstance(other, int):
+			return (self.Value < other)
+		return (self.Value < other.Value)
 
-    @property
-    def Value(self):
-        return self._value
-    @Value.setter
-    def Value(self, value):
-        self._value = value
+	def __gt__(self, other):
+		if isinstance(other, int):
+			return (self.Value > other)
+		return (self.Value > other.Value)
 
-    @property
-    def X(self):
-        return self._x
-    @X.setter
-    def X(self, value):
-        self._x = value
-    
-    @property
-    def Y(self):
-        return self._y
-    @Y.setter
-    def Y(self, value):
-        self._y = value
+	@property
+	def Value(self):
+		return self._value
+	@Value.setter
+	def Value(self, value):
+		self._value = value
 
-    """
-    En prévention pour algo A*
+	@property
+	def X(self):
+		return self._x
+	@X.setter
+	def X(self, value):
+		self._x = value
 
-    @property
-    def Gcost(self):
-        return self._gCost
-    @Gcost.setter
-    def Gcost(self, value):
-        self._gCost = value
-    @property
-    def Hcost(self):
-        return self._hCost
-    @Hcost.setter
-    def Hcost(self, value):
-        self._hCost = value
-    @property
-    def Fcost(self):
-        return self._gCost + self._hCost
-    """
+	@property
+	def Y(self):
+		return self._y
+	@Y.setter
+	def Y(self, value):
+		self._y = value
+
+	"""
+	En prévention pour algo A*
+
+	@property
+	def Gcost(self):
+		return self._gCost
+	@Gcost.setter
+	def Gcost(self, value):
+		self._gCost = value
+	@property
+	def Hcost(self):
+		return self._hCost
+	@Hcost.setter
+	def Hcost(self, value):
+		self._hCost = value
+	@property
+	def Fcost(self):
+		return self._gCost + self._hCost
+	"""
